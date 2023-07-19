63cc589a5a9f8ef19b535bb06707dcf264f58a0b
Martyanova K <martyanova-kseniya18@mail.ru>
2023-07-19 16:06:57 +0300

    new

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,19 +1,17 @@
 package web.controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.Servicelmpl;
-
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CarController {
     private static int CARS_COUNT;
-    @GetMapping(value = "/cars")
+    @GetMapping(value = "/")
     public String printWelcome(@RequestParam(value = "count", defaultValue = "5") int allCars, Model model) {
         List<Car> cars = new ArrayList<>();
         cars.add(new Car(++CARS_COUNT, "BMW", "dgip"));
