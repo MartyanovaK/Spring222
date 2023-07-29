ec7b6adf7996763a631ac685b03a2ec9e1f90bdf
Martyanova K <martyanova-kseniya18@mail.ru>
2023-07-29 13:06:09 +0300

    new

--- src/main/java/web/Dao/CarDao.java
+++ src/main/java/web/Dao/CarDao.java
@@ -3,17 +3,14 @@ import org.springframework.stereotype.Repository;
 import web.model.Car;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Repository
 public class CarDao {
-    private List<Car> cars = new ArrayList<Car>();
-
-    {
-        cars.add(new Car(1, "BMW", "dgip"));
-        cars.add(new Car(2, "Mersdes", "inomarka"));
-        cars.add(new Car(3, "ford", "miniven"));
-    }
+    private List<Car> cars = new ArrayList<Car>(Arrays.asList(new Car(1, "BMW", "dgip"),
+            new Car(2, "Mersdes", "inomarka"),
+            new Car(3, "ford", "miniven")));
     public List<Car> getCount(int count) {
         return cars.subList(0, count);
     }
